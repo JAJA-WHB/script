ae1401716eccc7806ff66b8b083455e5d3f895d3
JAJA-WHB <67504536+JAJA-WHB@users.noreply.github.com>
2023-06-26 15:26:13 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+    var searchContainer = document.getElementById('main');
+
+    // 设置居中样式
+    searchContainer.style.margin = '0 auto';
+    searchContainer.style.width = 'fit-content';
+
+    var searchBox = document.getElementById('tsf');
+
+    searchBox.style.margin = '0 auto';
